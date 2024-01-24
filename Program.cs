260ce278c0404a61ed697ad1e8be777688486524
Diana <diana.summer@mail.ru>
2024-01-24 18:13:47 +0700

    add first method

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿string[] strArray = new string [4]{ "Hello", "2", "world", ":-)" };
+string[] newArray = new string [strArray.Length];
+
+void CreateNewArray(string[] strArray, string[] newArray)
+{
+    int count = 0;
+    for (int i = 0; i < strArray.Length; i++)
+    {
+        if (strArray[i].Length <= 3)
+        {
+            newArray[count] = strArray[i];
+            count++;
+        }
+    }
+}
